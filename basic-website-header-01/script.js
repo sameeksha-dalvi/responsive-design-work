2d594e4850f0d1b868198b6346f4617199e58e9b
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-11-13 16:20:35 +0530

    feat: add click functionality of menu & close icon

--- basic-website-header-01/script.js
+++ basic-website-header-01/script.js
@@ -1,1 +1,16 @@
+const menu = document.querySelector("#hamburger-icon");
+const close = document.querySelector("#close-icon");
+const menuItems = document.querySelector(".menu-items");
 
+menu.addEventListener("click", function () {
+    menu.style.display = "none";
+    close.style.display = "block";
+    menuItems.style.display = "block";
+    //menuItems.setAttribute(display,"block");
+});
+
+close.addEventListener("click", function () {
+    menu.style.display = "block";
+    close.style.display = "none";
+    menuItems.style.display = "none";
+});
